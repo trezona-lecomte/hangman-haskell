71cece5d7d1ebbc5595d3e275afd561618ec0159
Kieran Trezona-le Comte <kieran@powershop.co.nz>
2016-05-12 22:51:46 +1200

    Set up random word generation

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,34 @@
 module Main where
 
-import Lib
+import Control.Monad (forever)
+import Data.Char (toLower)
+import Data.Maybe (isJust)
+import Data.List (intersperse)
+import System.Exit (exitSuccess)
+import System.Random (randomRIO)
+
+type WordList = [String]
+
+allWords :: IO WordList
+allWords = do
+  dict <- readFile "data/dict.txt"
+  return (lines dict)
+
+validLength :: Foldable t => t a -> Bool
+validLength w = length w `elem` [5..9]
+
+gameWords :: IO WordList
+gameWords = do
+  aw <- allWords
+  return (filter validLength aw)
+
+randomWord :: WordList -> IO String
+randomWord wordList = do
+  randomIndex <- randomRIO (0, length wordList - 1)
+  return $ wordList !! randomIndex
+
+randomWord' :: IO String
+randomWord' = gameWords >>= randomWord
 
 main :: IO ()
-main = someFunc
+main = undefined
